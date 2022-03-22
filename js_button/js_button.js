d5f4088c94a50ae5fc83dc7445ad263c62e36088
Alexandra (Aleks) Hatfield <22735580+aleks-hat@users.noreply.github.com>
2022-03-22 18:29:56 -0400

    Update js_button.js

--- js_button/js_button.js
+++ js_button/js_button.js
@@ -1,1 +1,9 @@
 //Basic clickable button, when clicked gives a Hello! alert
+
+function hello() {
+  alert("Hello");
+}
+window.onload = function() {
+  let btn = document.getElementById("myButton");
+  btn.onclick = hello;
+};
