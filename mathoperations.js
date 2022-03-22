124248b69aad1c0d9c8466acbfe688361387e8d7
Alexandra (Aleks) Hatfield <22735580+aleks-hat@users.noreply.github.com>
2022-03-22 16:26:20 -0400

    Update mathoperations.js

--- mathoperations.js
+++ mathoperations.js
@@ -1,1 +1,19 @@
 // Basic example of math operations in JavaScript
+
+let x = 18;
+let y = 32;
+
+//addition
+console.log(x+y);
+
+//subtraction
+console.log(x-y);
+
+//multiplication
+console.log(x*y);
+
+//division
+console.log(x/y);
+
+//modulo (remainder)
+console.log(x%y);
