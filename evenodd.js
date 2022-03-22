c6023e5db7a45918fbd366a500f0b46c8a7ed9a4
Alexandra (Aleks) Hatfield <22735580+aleks-hat@users.noreply.github.com>
2022-03-22 16:58:43 -0400

    Update evenodd.js

--- evenodd.js
+++ evenodd.js
@@ -1,1 +1,6 @@
-//Basic Javascript example for prining all odd or even numbers in a loop
+//Basic JavaScript example for printing all odd or even numbers in a loop
+
+//print even
+for(let i=0; i<=20; i+=2) {
+  console.log(i);
+} 
