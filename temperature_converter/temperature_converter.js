b7f6b6a7269a2c05e140f302e33ceeab3da0b916
Alexandra (Aleks) Hatfield <22735580+aleks-hat@users.noreply.github.com>
2022-03-22 20:01:38 -0400

    Update temperature_converter.js

--- temperature_converter/temperature_converter.js
+++ temperature_converter/temperature_converter.js
@@ -1,1 +1,14 @@
 //JS file for temperature converter, html and css files also in folder
+
+window.onload = function() {
+  let c = document.getElementById("celsius");
+  let f = document.getElementById("fahr");
+  //celcius formula
+  c.oninput = function() {
+      f.value = (c.value * 9/5) + 32;
+  };
+  //fahrenheit formula
+  f.oninput = function() {
+      c.value = (f.value - 32) * 5/9;
+  }
+};
